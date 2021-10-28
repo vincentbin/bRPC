db377d9baf040236960477eac5ff77839c8c5547
vincentAffinity <yanyibin@jmgo.com>
2021-10-29 02:27:34 +0800

    interceptor comment

--- netty-client/src/main/java/com/polyu/rpc/client/interceptor/Interceptor.java
+++ netty-client/src/main/java/com/polyu/rpc/client/interceptor/Interceptor.java
@@ -7,9 +7,22 @@ import com.polyu.rpc.client.invoke.Invocation;
  */
 public interface Interceptor {
 
+    /**
+     * 前处理
+     * @param invocation invoke 实例
+     */
     void beforeInvoke(Invocation invocation);
 
+    /**
+     * 拦截调用
+     * @param invocation invoke 实例
+     * @return request result
+     */
     Object intercept(Invocation invocation);
 
+    /**
+     * 后处理
+     * @param invocation invoke 实例
+     */
     void afterInvoke(Invocation invocation);
 }
