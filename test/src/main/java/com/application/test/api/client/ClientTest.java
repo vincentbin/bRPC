e90a3be7cd2a9dc8d06389d7c6e00dca85d77086
vincent <527500791@qq.com>
2022-05-28 09:05:03 +0800

    version

--- test/src/main/java/com/application/test/api/client/ClientTest.java
+++ test/src/main/java/com/application/test/api/client/ClientTest.java
@@ -4,13 +4,17 @@ import com.application.test.service.HelloService;
 import com.polyu.rpc.client.RpcClient;
 import com.polyu.rpc.registry.zookeeper.ZKDiscovery;
 import com.polyu.rpc.route.impl.RpcLoadBalanceRoundRobin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClientTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientTest.class);
 
     public static void main(String[] args) throws Exception {
         new RpcClient(new ZKDiscovery("127.0.0.1:2181", "testYYB"));
         HelloService helloService = RpcClient.getProxyInstance(HelloService.class, "1.0", new RpcLoadBalanceRoundRobin(), 3000L);
         String str = helloService.hello("yyb");
-        System.out.println("str = " + str);
+        logger.info("str = {}", str);
     }
 }
